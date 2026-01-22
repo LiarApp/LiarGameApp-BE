aa7d56d97f7a6df41b0b2d2027bb579a4eefd5f4
ekdus04 <dygim04@gmail.com>
2026-01-22 17:53:09 +0900

    setting: spring initialize

---
+++ src/main/java/com/liargame/backend/LiargameBackendApplication.java
@@ -0,0 +1,13 @@
+package com.liargame.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiargameBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiargameBackendApplication.class, args);
+	}
+
+}
