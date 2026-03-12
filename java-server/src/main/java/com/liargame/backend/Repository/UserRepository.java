1c33e0124db6ad820765d99021134fdbca79048e
202310957 <snu11025@naver.com>
2026-03-12 14:09:52 +0900

    refactor: 닉네임, 프로필 이미지 업데이트 로직 오류 수정

--- java-server/src/main/java/com/liargame/backend/Repository/UserRepository.java
+++ java-server/src/main/java/com/liargame/backend/Repository/UserRepository.java
@@ -18,8 +18,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     // 사용자 닉네임, 프로필 이미지를 업데이트합니다.
     @Modifying
     @Query("UPDATE User u " +
-            "SET u.nickname = :nickname, u.profileImg = :profileImg " +
-            "WHERE u.id =: id"
+            "SET u.nickname = COALESCE(:nickname, u.nickname), " +
+            "u.profileImg = COALESCE(:profileImg, u.profileImg) " +
+            "WHERE u.id = :id"
     )
     int updateUserInfo(
         @Param("id") Long id,
